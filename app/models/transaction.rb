defab52c7b7f0f70b2e1e71e1860f4a063522c2f
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-12-19 23:26:01 +0100

    Setup associations in models

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,11 @@
 class Transaction < ApplicationRecord
+    belongs_to :user
+    has_many :transaction_categories, foreign_key: 'transaction_id', dependent: :destroy
+    has_many :categories, through: :transaction_categories, foreign_key: 'transaction_id'
+end
+
+# Path: app\models\transaction.rb
+class Transaction
     validate :name, presence: true, length: { in: 3..20 }
     validates :amount, presence: true, numericality: { greater_than: 0 }
 end
