bbe95d96faa39cf88ec5d76deb19a123a68d71c0
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-12-20 22:41:34 +0100

    Setup linters and fix all linter errors

--- app/models/transaction_category.rb
+++ app/models/transaction_category.rb
@@ -1,4 +1,4 @@
 class TransactionCategory < ApplicationRecord
-    belongs_to :transaction
-    belongs_to :category
+  belongs_to :transaction
+  belongs_to :category
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ApplicationRecord
-  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    has_many :transactions, foreign_key: 'user_id', dependent: :destroy
-    has_many :categories, foreign_key: 'user_id', dependent: :destroy
+  has_many :transactions, foreign_key: 'user_id', dependent: :destroy
+  has_many :categories, foreign_key: 'user_id', dependent: :destroy
 
-    validates :username, presence: true, length: { in: 3..20 }, uniqueness: true
-    validates :name, presence: true, length: { in: 3..20 }
+  validates :username, presence: true, length: { in: 3..20 }, uniqueness: true
+  validates :name, presence: true, length: { in: 3..20 }
 end
