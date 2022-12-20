04d95e4e4957a786c823ada027839d6eb71d2f60
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-12-20 23:08:56 +0100

    Add changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   has_many :transactions, foreign_key: 'user_id', dependent: :destroy
   has_many :categories, foreign_key: 'user_id', dependent: :destroy
 
-  validates :username, presence: true, length: { in: 3..20 }, uniqueness: true
   validates :name, presence: true, length: { in: 3..20 }
+  validates :email, presence: true, length: { in: 3..20 }
 end
