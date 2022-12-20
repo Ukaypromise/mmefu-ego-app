9353923448e251e7771f0b03485d46cd008798c1
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-12-20 22:25:17 +0100

    Setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :transaction_categories
+  resources :transactions
+  resources :categories
+  resources :splash
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root "splash#index"
+  devise_scope :user do 
+    get '/users/sign_out' => 'devise/sessions#destroy' 
+  end
 end
